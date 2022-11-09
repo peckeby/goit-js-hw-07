ef7ad0482c00d243c5573be7c4350831710002ba
Vlad Luchkov <unrasend@gmail.com>
2022-11-09 19:49:19 +0000

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,31 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallUrl = [...document.querySelectorAll(".gallery__item")];
-
-const gallImgR = [...document.querySelectorAll(".gallery__image")];
+const galleryItems = [...document.querySelectorAll(".gallery__item")];
 
 const setAttributes = (el, attrs) => Object.entries(attrs).forEach(([key, value]) => el.setAttribute(key, value));
 
-const getUrl = () =>
-    galleryItems.forEach(item =>
-        gallImgR.forEach (img => 
-            gallImgR.indexOf(img) === galleryItems.indexOf(item) 
-            ? setAttributes(img, {"src": `${item.preview}`, "alt": `${item.description}`}) 
-            : false            
-            ));
-    galleryItems.forEach(item =>
-        gallUrl.forEach (a => 
-            gallUrl.indexOf(a) === galleryItems.indexOf(item) 
-            ? a.setAttribute("href", `${item.original}`) 
-            : false
-        )
-    );
+galleryItems.forEach((item, index) => {
+  setAttributes(item.firstElementChild, { src: `${item.preview}`, alt: `${item.description}`}));
+  item.setAttribute('href', `${item.original}`) 
+});
 
-getUrl();
-
-let gallery = new SimpleLightbox('.gallery .gallery__item', {
-    captionsData: 'alt',
-    captionPosition: 'bottom',
-    captionDelay: '250ms',
+const gallery = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250ms',
 });
