276a8eedc1e78e29212a7e6f03a3f9963945adb0
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-11-09 23:27:24 +0200

    Refactor code with comments

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallUrl = [...document.querySelectorAll(".gallery__item")];
+const gallery = document.querySelector(".gallery");
+const galleryLink = galleryItems
+.map((item) => `<a class="gallery__item" href="${item.original}">
+<img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`)
+.join("");
 
-const gallImgR = [...document.querySelectorAll(".gallery__image")];
+gallery.insertAdjacentHTML("afterbegin", galleryLink);
 
-const setAttributes = (el, attrs) => Object.entries(attrs).forEach(([key, value]) => el.setAttribute(key, value));
-
-const getUrl = () =>
-    galleryItems.forEach(item =>
-        gallImgR.forEach (img => 
-            gallImgR.indexOf(img) === galleryItems.indexOf(item) 
-            ? setAttributes(img, {"src": `${item.preview}`, "alt": `${item.description}`}) 
-            : false            
-            ));
-    galleryItems.forEach(item =>
-        gallUrl.forEach (a => 
-            gallUrl.indexOf(a) === galleryItems.indexOf(item) 
-            ? a.setAttribute("href", `${item.original}`) 
-            : false
-        )
-    );
-
-getUrl();
-
-let gallery = new SimpleLightbox('.gallery .gallery__item', {
+let galleryN = new SimpleLightbox('.gallery .gallery__item', {
     captionsData: 'alt',
     captionPosition: 'bottom',
     captionDelay: '250ms',
